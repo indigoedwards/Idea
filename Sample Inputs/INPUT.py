8f8cb20215b6c232be29ba0669e0b669c566f141
indigoedwards <indigo.edwards@york.ac.uk>
2024-11-11 13:09:43 +0000

    finalised inputs

--- Sample Inputs/INPUT.py
+++ Sample Inputs/INPUT.py
@@ -11,8 +11,13 @@ job = "assemble" #"find" #"plotpotential"
 
 #=============GENERAL SETTINGS===================
 #X-GRID SETTINGS
-xgrid_maxrange = 30
+xgrid_start = -30
+xgrid_end = 30
 xgrid_points = 300
+#FILE DIRECTORIES
+output_file = ""
+save_state_directory = ""
+save_graph_directory = ""
 
 #============ASSEMBLE SETTINGS===================
 #WELL MOVING SETTINGS
@@ -20,20 +25,26 @@ xgrid_points = 300
 starting_distance = 20
 distance_step = 0.1
 inner_prod_tolerance = 0.1
-
-#================FIND SETTINGS===================
+#OUTPUT SETTINGS
+enable_gif_generation = True
+gif_observables = ["density","wavefunction","energy","exch-energy","kin-energy"]
+graph_final_state = True
+final_state_graphs = ["density","wavefunction","exch-energy","kin-energy"]      
+#FINDING EXCITATION SETTINGS
 enable_finding_double = True
 known_double_excitation = 7 #only needed if enable_finding_double=False
+
+#================FIND SETTINGS===================
 excitation_limit = 15
 peakfinder_sensitivity = 20
-
-#================OUTPUT SETTINGS=================
 #OUTPUT SETTINGS
-enable_gif_generation_density = True        
-enable_gif_generation_wavefunction = True   
-enable_gif_generation_energy = True         
-enable_final_energy_graph = True            
-save_all_states = True                      
+
+#==========GENERAL OUTPUTS======================
+output_observables = True     
+output_energy = True
+save_states = True
+save_final_energy_graph = True
+               
 
 
 
