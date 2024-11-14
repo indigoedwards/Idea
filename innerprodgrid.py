e19f318a51e5c85466aa27bb8d7273e7823d3a39
indigoedwards <indigo.edwards@york.ac.uk>
2024-11-14 16:13:54 +0000

    fixed innerprod

--- innerprodgrid.py
+++ innerprodgrid.py
@@ -7,7 +7,7 @@ import iDEA as idea
 #RETURN:    inner product of states
 
 def innerproduct(state1,state2,system1,system2):    
-    return abs(np.sum(state1.full*state2.full)*system1.dx*system2.dx)
+    return abs(np.sum(state1*state2)*system1.dx*system2.dx)
 
 
 #Generate the innerproduct grid for two states
@@ -15,6 +15,6 @@ def innerprodgrid(state1,state2,system1,system2,maxexcitation):
     grid = np.zeros((maxexcitation,maxexcitation))
     for i in range(0,maxexcitation):
         for j in range(0,maxexcitation):
-            grid[i][j] = innerproduct(state1,state2,system1,system2)
+            grid[i][j] = innerproduct(state1.allfull[...,i],state2.allfull[...,j],system1,system2)
     
     return grid 
