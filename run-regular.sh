40a2da3a82227ed181b9575381bb18420ebc9734
indigoedwards <indigo.edwards@york.ac.uk>
2024-11-26 10:34:44 +0000

    fixed run script

--- run-regular.sh
+++ run-regular.sh
@@ -1,5 +1,4 @@
-#!/usr/bin/bash
+ #!/usr/bin/bash
 
 source ~/venv/bin/activate
-script_return=$(OMP_NUM_THREADS=13 python3 -u main.py)
-echo "$script_return" > ../log.txt
+OMP_NUM_THREADS=13 python3 -u main.py > log.txt
